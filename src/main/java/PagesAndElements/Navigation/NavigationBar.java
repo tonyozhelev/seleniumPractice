3b3871308accc7aef4795c378339b9ee0e762cfd
Tonyo <tonyo_zhelev@abv.bg>
2019-02-05 10:39:55 +0200

    File reading extracted in separate class

--- src/main/java/PagesAndElements/Navigation/NavigationBar.java
+++ src/main/java/PagesAndElements/Navigation/NavigationBar.java
@@ -2,48 +2,13 @@ package PagesAndElements.Navigation;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import static Utils.Browser.driver;
+import static Utils.FileUtils.getTestData;
 
 public class NavigationBar
 {
-    /**
-     * method for extracting data from csv file.
-     * @param filename name of file
-     * @param delimiter delimiter of file
-     * @return String[][] - first dimention - rows of file, second dimension - elements in each row
-     * @throws IOException thrown if file is invalid
-     */
-    private static String[][] getTestData(String filename, String delimiter) throws IOException {
-        List<String[]> records = new ArrayList<>();
-        String record;
 
-        BufferedReader file = new BufferedReader(new FileReader("src\\main\\resources\\dataFiles\\" + filename));
-        record = file.readLine();
-        while (record != null) {
-            String[] fields = record.split(delimiter);
-            records.add(fields);
-            record = file.readLine();
-        }
-
-        file.close();
-
-        return records.toArray(new String[records.size()][]);
-    }
-
-    /**
-     * method for extracting data from csv file.
-     * @param filename name of file
-     * @return String[][] - first dimention - rows of file, second dimension - elements in each row
-     * @throws IOException thrown if file is invalid
-     */
-    private static String[][] getTestData(String filename) throws IOException {
-        return getTestData(filename,",");
-    }
 
 
     /**
